3057dd5b722505555feb4d45f9fe5308f770dcd7
Julianmost <67060277+Julianmost@users.noreply.github.com>
2020-07-07 13:31:04 +0200

    test

--- gui/TestGui.java
+++ gui/TestGui.java
@@ -4,7 +4,7 @@ public class TestGui {
     public static void main(String[] args) {
         //new Hauptmenue("Hauptmenue");
         //new Spielauswahl("Spielmodus");
-        new GuiMuendlicheAntwortFrage("Frage", "Hier koennte ihre Frage stehen", "Hier koennte ihre Loesung stehen");
-        //new GuiVierAntwortFrage("Frage", "Hier steht der Fragetext", "Richtige Antwort, die aber viel zu lang ist", "Falsche Antwort 1", "Falsche Antwort 2", "Falsche Antwort 3");
+        //new GuiMuendlicheAntwortFrage("Frage", "Hier koennte ihre Frage stehen", "Hier koennte ihre Loesung stehen");
+        new GuiVierAntwortFrage("Frage", "Hier steht der Fragetext", "Richtige Antwort, die aber viel zu lang ist", "Falsche Antwort 1", "Falsche Antwort 2", "Falsche Antwort 3");
     }
 }
